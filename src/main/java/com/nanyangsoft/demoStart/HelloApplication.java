c38d8a7dca4eab96b0f9500d1328a943c0dea3d6
fusxian <870086043@qq.com>
2018-01-06 21:14:39 +0800

    Initial commit

---
+++ src/main/java/com/nanyangsoft/demoStart/HelloApplication.java
@@ -0,0 +1,25 @@
+package com.nanyangsoft.demoStart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Lenovo on 2018/1/6.
+ */
+@RestController()
+@SpringBootApplication
+public class HelloApplication {
+    public static  void main(String[] args){
+        SpringApplication.run(HelloApplication.class,args);
+    }
+
+    @RequestMapping(method= RequestMethod.GET,path = "/hello")
+    @ResponseBody
+    public  String hello(){
+        return "hello";
+    }
+}
